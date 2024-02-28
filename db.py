68b36104555ae4c57ecb8e67a81f0c4047be41a5
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-02-28 21:35:43 +0300

    feat: class based

---
+++ db.py
@@ -0,0 +1,48 @@
+
+import sqlite3
+import json
+
+
+def create_table():
+    conn = sqlite3.connect('channel_links.db')
+    cursor = conn.cursor()
+
+    # Create a table with a column to store JSON documents
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS json_data (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            data JSON UNIQUE
+        )
+    ''')  
+
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
+
+
+
+def insert_json_data(json_data):
+    conn = sqlite3.connect('channel_links.db')
+    cursor = conn.cursor()    
+
+    # Insert the JSON data into the table
+    cursor.execute('INSERT INTO json_data (data) VALUES (?)', (json.dumps(json_data),))
+
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
+
+
+def get_all_data(data):
+    conn = sqlite3.connect('channel_links.db')
+    cursor = conn.cursor()    
+    # Retrieve all rows from the table
+    cursor.execute(f'SELECT data FROM {data}')
+    rows = cursor.fetchall()
+
+    # Convert the JSON data back to Python objects
+    result = [json.loads(row[0]) for row in rows]
+
+    # Close the connection
+    conn.close()
+    return result
--- main.py
+++ main.py
@@ -1,127 +1,27 @@
 import telebot
 from telebot import util, types
 import time
-from utils import extract_data, format
 import os
-from logger import LOGGER
+from logging import getLogger, basicConfig, INFO, StreamHandler, FileHandler
+from poster import RSSPoster
+from db import *
+
+basicConfig(
+    format="%(asctime)s | %(levelname)s | %(module)s - line_no %(lineno)s : %(message)s ",
+    handlers=[
+        StreamHandler(),
+        FileHandler('./logs.txt')],
+    level=INFO)
+logger = getLogger(__name__)
+
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN, parse_mode="HTML")
+
+poster = RSSPoster()
+create_table()
 
 
-def get_domain_from_url(url):
-    try:
-        # Split the URL by "//" and take the second part
-        url = url.replace("www", "")
-        domain = url.split("//")[1].split("/")[0]
-        return domain.replace(".com", "").split(".")[0]
-    except Exception as e:
-        LOGGER.error(f"Error {e} extracting from {url}:")
-        return None
 
-
-def send_data(entries, domain, chat_id, no_of_links):
-    for entry in entries:
-        message, content = format(entry)
-        not_sent = update_files(path=f"./db/{domain}.txt", link=entry["link"])
-        if not_sent:
-            try:
-                markup = types.InlineKeyboardMarkup()
-                button = types.InlineKeyboardButton("Read More", url=content)
-                markup.add(button)
-                sent_message = bot.send_message(
-                    chat_id, message, reply_markup=markup, parse_mode="html")
-                message_data = {"id": sent_message.id, "content": content}
-                messages_content.append(message_data)
-                time.sleep(2)
-                no_of_links += 1
-            except Exception as e:
-                LOGGER.error(f"Error '{e}' occured when sending {domain} data")
-    LOGGER.info(f"{domain.title()} updated successfully")
-    return no_of_links
-
-
-def to_update(items, message, no_of_links=0):
-    for (key, value) in items.items():
-        domain = key
-        url = value["feed"]
-        chat_id = value["chat_id"]
-        try:
-            data = extract_data(url)
-        except Exception as e:
-            LOGGER.error(f"Error {e} when extracting data from {domain}")
-        bot.edit_message_text(
-            f"Updating {domain.title()}",
-            message.chat.id,
-            message.id)
-        try:
-            links = send_data(data, domain, chat_id, no_of_links)
-            no_of_links += links
-            bot.edit_message_text(
-                f"Updated {domain.title()} Successfully!. Links sent {no_of_links}",
-                message.chat.id,
-                message.id)
-        except Exception as e:
-            LOGGER.error(f"Error '{e}' occurred when sending {domain} data")
-            bot.edit_message_text(
-                f"Updated {domain} failed!. Error: {e}",
-                message.chat.id,
-                message.id)
-    return no_of_links
-
-
-def update_files(path, link):
-    processed_urls = ""
-    if os.path.exists(path):
-        with open(path, 'r') as file:
-            processed_urls = file.read()
-    else:
-        with open(path, 'w') as file:
-            processed_urls = file.write("\n")
-    if link not in processed_urls:
-        with open(path, 'a') as file:
-            file.write(f"\n{link}")
-        return True
-    return False
-
-
-TELGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
-
-FEEDS = [
-    {
-        "chat_id": -1002103516422, "url": "https://billboard.com/feed/"
-    },
-    {
-        "chat_id": -1001908193905, "url": "https://citizen.digital/feed.xml"
-    },
-    {
-        "chat_id": -1002056978598, "url": "https://animecorner.me/feed/"
-    },
-    {
-        "chat_id": -1002042618375, "url": "https://soompi.com/feed"
-    },
-    {
-        "chat_id": -1002139940705, "url": "https://tmz.com/rss.xml"
-    },
-    {
-        "chat_id": 1095126805, "url": "https://animesenpai.net/feed/"
-    },
-    {
-        "chat_id": -1002011684310, "url": "https://variety.com/feed/"
-    }
-]
-
-
-urls = [feed["url"] for feed in FEEDS]
-chat_ids = [feed["chat_id"] for feed in FEEDS]
-domains = [get_domain_from_url(url) for url in urls]
-items = {}
-for idx, channel in enumerate(domains):
-    items[channel] = {
-        "feed": urls[idx],
-        "chat_id": chat_ids[idx],
-    }
-messages_content = []
-
-
-bot = telebot.TeleBot(TELGRAM_BOT_TOKEN, parse_mode="HTML")
 @bot.message_handler(commands=["start"])
 def hello(message):
     bot.send_message(message.chat.id, f"Hello {message.from_user.first_name}")
@@ -129,7 +29,7 @@ def hello(message):
 
 @bot.message_handler(commands=["logs"])
 def hello(message):
-    with open('logs.txt')as file:
+    with open('logs.txt') as file:
         text = file.read()
     splitted_text = util.smart_split(text, 4096)
     for text_instance in splitted_text:
@@ -138,29 +38,36 @@ def hello(message):
         file.write("")
 
 
-@bot.message_handler(commands=["stop"])
-def stop(message=None):
-    global infinity
-    infinity = False
-    bot.send_message(message.chat.id, "Infinity stoped")
-
-
 @bot.message_handler(commands=["update"])
 def update(message=None):
-    chat_id = 1095126805 if message is None else message.chat.id
-    message = bot.send_message(chat_id, "Updating channels ⏳...")
-    no_of_links = to_update(items, message)
-    bot.edit_message_text(
-        f"Completed ✅ total links sent {no_of_links}",
-        message.chat.id,
-        message.id)
+    message = bot.reply_to(message, "Updating channels ⏳...")
+    feeds = poster.FEEDS
     no_of_links = 0
+    for feed in feeds:
+        messages = poster.get_messages(feed)
+        for item in messages:
+            markup = types.InlineKeyboardMarkup()
+            button = types.InlineKeyboardButton("Read More", url=item["url"])
+            markup.add(button)
+            try:
+                insert_json_data(item)
+            except:
+                continue
+            bot.send_message(
+            feed["chat_id"], item["text"], reply_markup=markup)
+            time.sleep(60)
+            no_of_links += 1
+            time.sleep(2)
+            break
 
+        bot.edit_message_text(
+            f"Completed ✅ total links sent {no_of_links}",
+            message.chat.id,
+            message.id)
+        no_of_links = 0
 
-def start_bot(bot):
-    LOGGER.info("Bot online")
-    bot.polling()
 
 
 if __name__ == "__main__":
-    start_bot(bot)
+    logger.info("Bot online")
+    bot.polling()
