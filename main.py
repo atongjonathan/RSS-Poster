84aca35e5820fb7f714941c6a76aeef8840f39e2
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-02-29 09:34:45 +0300

    files rename

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 from logging import getLogger, basicConfig, INFO, StreamHandler, FileHandler
 from poster import RSSPoster
 from mongodb import Database
+import os
 
 # from db import *
 
@@ -15,7 +16,7 @@ basicConfig(
     level=INFO)
 logger = getLogger(__name__)
 
-TELEGRAM_BOT_TOKEN = "5954527089:AAHQJGcyGaI_MfT6DsoEgmKicfjBujizCbA"
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN, parse_mode="HTML")
 
 poster = RSSPoster()
