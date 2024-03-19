4d033e392da1fb49e146c0fc25b7c7157afc7573
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-03-19 20:24:45 +0300

    fix __str__()

--- main.py
+++ main.py
@@ -73,7 +73,11 @@ def update(message=None):
                             feed["chat_id"], text=split_message, reply_markup=markup)
                     except Exception as e:
                         if "429" in str(e):
-                            duration = int(str(e[-2:]))
+                            duration = int(e.__str__()[-2:])
+                        bot.edit_message_text(
+            f"Update paused for {duration}s due to too many messages, for {feed['domain']} will resume shortly...",
+            message.chat.id,
+            message.id)
                         logger.error(
                             f"An error occured when sending smart split :'{e}'\n Sleep in {duration}")
                         time.sleep(duration)
